2ce3b57b5a522339f44febfaea7ba179ddb55f38
odalebrown <odale_brown@yahoo.com>
2020-04-20 19:50:09 -0400

    index.html change:

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -10,12 +10,12 @@ Vue.component('app-header', {
 
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                  <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">News</a>
-                  </li>
+                    <li class="nav-item active">
+                        <router-link to="/" class="nav-link">Home</router-link>
+                    </li>
+                    <li class="nav-item active">
+                       <router-link to="/news" class="nav-link">News</router-link>
+                    </li>
                 </ul>
               </div>
             </nav>
